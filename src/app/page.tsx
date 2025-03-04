a1a5aa000005aa7377e90b053e741d4ca14420aa
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-03-04 16:22:58 -0300

    Adicione componente Button com estilos e configurações.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+  return (<main>
+    <div>Hello world!</div>
+    <Button variant='outline'>Clique aqui</Button>
+  </main>);
 }
