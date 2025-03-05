6ce2baba1a698b66ebaebf533719b5168a92429a
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-03-05 12:09:06 -0300

    Adiciona configuração ESLint e ajustes no código para padrões

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,388 +1,388 @@
 const issItems = [
-  { text: "Emitir Guia ISS/TLFF/TLA/TRIFS", href: "#" },
+  { text: 'Emitir Guia ISS/TLFF/TLA/TRIFS', href: '#' },
   {
-    text: "Emitir Guia ISS/TLFF/TLA/TRIFS Autônomo",
-    href: "#",
+    text: 'Emitir Guia ISS/TLFF/TLA/TRIFS Autônomo',
+    href: '#',
   },
   {
-    text: "Nota Fiscal de Serviços",
-    href: "#",
+    text: 'Nota Fiscal de Serviços',
+    href: '#',
   },
   {
-    text: "Nota Fiscal de Serviços Avulsa",
-    href: "#",
+    text: 'Nota Fiscal de Serviços Avulsa',
+    href: '#',
   },
   {
-    text: "Emitir Cartão de Inscrição de Empresa/Autônomo",
-    href: "#",
+    text: 'Emitir Cartão de Inscrição de Empresa/Autônomo',
+    href: '#',
   },
   {
-    text: "Certidão Conjunta Negativa de Débitos Municipais e da Dívida Ativa do Município",
-    href: "#",
+    text: 'Certidão Conjunta Negativa de Débitos Municipais e da Dívida Ativa do Município',
+    href: '#',
   },
   {
-    text: "Certidão Conjunta Positiva de Débitos Municipais e da Dívida Ativa do Município",
-    href: "#",
+    text: 'Certidão Conjunta Positiva de Débitos Municipais e da Dívida Ativa do Município',
+    href: '#',
   },
-  { text: "Reemitir Certidão/Declaração", href: "#" },
-  { text: "Validar Certidão/Declaração", href: "#" },
-  { text: "Certidão de Baixa", href: "#" },
-  { text: "Certidão de Estimativa", href: "#" },
-  { text: "Consultar Alvará de Funcionamento", href: "#" },
+  { text: 'Reemitir Certidão/Declaração', href: '#' },
+  { text: 'Validar Certidão/Declaração', href: '#' },
+  { text: 'Certidão de Baixa', href: '#' },
+  { text: 'Certidão de Estimativa', href: '#' },
+  { text: 'Consultar Alvará de Funcionamento', href: '#' },
   {
-    text: "Declaração de Inexistência de Cadastro Mercantil",
-    href: "#",
+    text: 'Declaração de Inexistência de Cadastro Mercantil',
+    href: '#',
   },
   {
-    text: "Portal de Parcelamento e Negociação",
-    href: "#",
+    text: 'Portal de Parcelamento e Negociação',
+    href: '#',
   },
-  { text: "Legislação", href: "#" },
+  { text: 'Legislação', href: '#' },
   {
-    text: "Notificação de Instrução Normativa",
-    href: "#",
+    text: 'Notificação de Instrução Normativa',
+    href: '#',
   },
-];
+]
 
 const iptuItems = [
-  { text: "Certidão Negativa de Débitos de IPTU", href: "#" },
+  { text: 'Certidão Negativa de Débitos de IPTU', href: '#' },
   {
-    text: "Certidão Positiva com Efeitos de Negativa IPTU",
-    href: "#",
+    text: 'Certidão Positiva com Efeitos de Negativa IPTU',
+    href: '#',
   },
   {
     text:
-      "Emitir Guia IPTU/TCRD-Taxa de Lixo/COSIP -\n" +
-      "Taxa de Iluminação Pública",
-    href: "#",
+      'Emitir Guia IPTU/TCRD-Taxa de Lixo/COSIP -\n' +
+      'Taxa de Iluminação Pública',
+    href: '#',
   },
   {
-    text: "Emitir Memória de Cálculo do Imóvel",
-    href: "#",
+    text: 'Emitir Memória de Cálculo do Imóvel',
+    href: '#',
   },
   {
-    text: "Declaração de Inexistência de Imóvel",
-    href: "#",
+    text: 'Declaração de Inexistência de Imóvel',
+    href: '#',
   },
   {
-    text: "Reemitir Certidão/Declaração",
-    href: "#",
+    text: 'Reemitir Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Validar Certidão/Declaração",
-    href: "#",
+    text: 'Validar Certidão/Declaração',
+    href: '#',
   },
-  { text: "Portal do IPTU", href: "#" },
-  { text: "Portal de Negociação", href: "#" },
-];
+  { text: 'Portal do IPTU', href: '#' },
+  { text: 'Portal de Negociação', href: '#' },
+]
 
 const itbiItems = [
-  { text: "Emitir Guia ITBI-e", href: "#" },
+  { text: 'Emitir Guia ITBI-e', href: '#' },
   {
-    text: "Emitir Guia SIAT",
-    href: "#",
+    text: 'Emitir Guia SIAT',
+    href: '#',
   },
   {
-    text: "Sistema ITBI-e",
-    href: "#",
+    text: 'Sistema ITBI-e',
+    href: '#',
   },
   {
-    text: "Certidão Negativa de Transferência Imobiliária",
-    href: "#",
+    text: 'Certidão Negativa de Transferência Imobiliária',
+    href: '#',
   },
   {
-    text: "Reemitir Certidão/Declaração",
-    href: "#",
+    text: 'Reemitir Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Validar Certidão/Declaração",
-    href: "#",
+    text: 'Validar Certidão/Declaração',
+    href: '#',
   },
-];
+]
 
 const taxaItems = [
-  { text: "Emitir Guia ISS/TLFF/TLA/TRIFS", href: "#" },
+  { text: 'Emitir Guia ISS/TLFF/TLA/TRIFS', href: '#' },
   {
-    text: "Emitir Guia ISS/TLFF/TLA/TRIFS Autonômo",
-    href: "#",
+    text: 'Emitir Guia ISS/TLFF/TLA/TRIFS Autonômo',
+    href: '#',
   },
   {
-    text: "Emitir Cartão de Inscrição de Empresa/Autônomo",
-    href: "#",
-  },
-  {
-    text:
-      "Certidão Conjunta Negativa de Débitos\n" +
-      "Municipais e da Dívida Ativa do Município",
-    href: "#",
+    text: 'Emitir Cartão de Inscrição de Empresa/Autônomo',
+    href: '#',
   },
   {
     text:
-      "Certidão Conjunta Positiva de Débitos\n" +
-      "Municipais e da Dívida Ativa do Município",
-    href: "#",
+      'Certidão Conjunta Negativa de Débitos\n' +
+      'Municipais e da Dívida Ativa do Município',
+    href: '#',
   },
   {
-    text: "Reemitir Certidão/Declaração",
-    href: "#",
+    text:
+      'Certidão Conjunta Positiva de Débitos\n' +
+      'Municipais e da Dívida Ativa do Município',
+    href: '#',
   },
   {
-    text: "Consultar Alvará de Funcionamento",
-    href: "#",
+    text: 'Reemitir Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Declaração de Inexistência de Cadastro Mercantil",
-    href: "#",
+    text: 'Consultar Alvará de Funcionamento',
+    href: '#',
   },
   {
-    text: "Portal de Parcelamento e Negociação",
-    href: "#",
+    text: 'Declaração de Inexistência de Cadastro Mercantil',
+    href: '#',
   },
   {
-    text: "Sistema de Licenciamento - SLIC",
-    href: "#",
+    text: 'Portal de Parcelamento e Negociação',
+    href: '#',
   },
-];
+  {
+    text: 'Sistema de Licenciamento - SLIC',
+    href: '#',
+  },
+]
 
 const imovelItens = [
-  { text: "Certidão Negativa de Débitos de IPTU", href: "#" },
+  { text: 'Certidão Negativa de Débitos de IPTU', href: '#' },
   {
-    text: "Certidão Positiva com Efeitos de Negativa IPTU",
-    href: "#",
+    text: 'Certidão Positiva com Efeitos de Negativa IPTU',
+    href: '#',
   },
   {
     text:
-      "Emitir Guia IPTU/TCRD-Taxa de Lixo/COSIP -\n" +
-      "Taxa de Iluminação Pública",
-    href: "#",
+      'Emitir Guia IPTU/TCRD-Taxa de Lixo/COSIP -\n' +
+      'Taxa de Iluminação Pública',
+    href: '#',
   },
   {
-    text: "Emitir Memória de Cálculo do Imóvel",
-    href: "#",
+    text: 'Emitir Memória de Cálculo do Imóvel',
+    href: '#',
   },
   {
-    text: "Emitir Guia ITBI-e",
-    href: "#",
+    text: 'Emitir Guia ITBI-e',
+    href: '#',
   },
   {
-    text: "Emitir Guia SIAT",
-    href: "#",
+    text: 'Emitir Guia SIAT',
+    href: '#',
   },
   {
-    text: "Sistema ITBI-e",
-    href: "#",
+    text: 'Sistema ITBI-e',
+    href: '#',
   },
   {
-    text: "Declaração de Inexistência de Imóvel",
-    href: "#",
+    text: 'Declaração de Inexistência de Imóvel',
+    href: '#',
   },
   {
-    text: "Reemitir Certidão/Declaração",
-    href: "#",
+    text: 'Reemitir Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Validar Certidão/Declaração",
-    href: "#",
+    text: 'Validar Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Portal do IPTU",
-    href: "#",
+    text: 'Portal do IPTU',
+    href: '#',
   },
   {
-    text: "Portal de Negociação",
-    href: "#",
+    text: 'Portal de Negociação',
+    href: '#',
   },
   {
-    text: "Sistema Coleta de Lixo",
-    href: "#",
+    text: 'Sistema Coleta de Lixo',
+    href: '#',
   },
   {
-    text: "Sistema de Coleta de Poda",
-    href: "#",
+    text: 'Sistema de Coleta de Poda',
+    href: '#',
   },
   {
-    text: "Certidão de Número",
-    href: "#",
+    text: 'Certidão de Número',
+    href: '#',
   },
   {
-    text: "Declaração de Rua",
-    href: "#",
+    text: 'Declaração de Rua',
+    href: '#',
   },
   {
-    text: "Construa Fácil",
-    href: "#",
+    text: 'Construa Fácil',
+    href: '#',
   },
-];
+]
 
 const empresaItens = [
-  { text: "Emitir Guia ISS/TLFF/TLA/TRIFS", href: "#" },
+  { text: 'Emitir Guia ISS/TLFF/TLA/TRIFS', href: '#' },
   {
-    text: "Emitir Guia ISS/TLFF/TLA/TRIFS Autonômo",
-    href: "#",
+    text: 'Emitir Guia ISS/TLFF/TLA/TRIFS Autonômo',
+    href: '#',
   },
   {
-    text: "Nota Fiscal de Serviços",
-    href: "#",
+    text: 'Nota Fiscal de Serviços',
+    href: '#',
   },
   {
-    text: "Nota Fiscal de Serviços Avulsa",
-    href: "#",
+    text: 'Nota Fiscal de Serviços Avulsa',
+    href: '#',
   },
   {
-    text: "Emitir Cartão de Inscrição de Empresa/Autônomo",
-    href: "#",
-  },
-  {
-    text:
-      "Certidão Conjunta Negativa de Débitos\n" +
-      "Municipais e da Dívida Ativa do Município",
-    href: "#",
+    text: 'Emitir Cartão de Inscrição de Empresa/Autônomo',
+    href: '#',
   },
   {
     text:
-      "Certidão Conjunta Negativa de Débitos\n" +
-      "Municipais e da Dívida Ativa do Município",
-    href: "#",
+      'Certidão Conjunta Negativa de Débitos\n' +
+      'Municipais e da Dívida Ativa do Município',
+    href: '#',
   },
   {
-    text: "Reemitir Certidão/Declaração",
-    href: "#",
+    text:
+      'Certidão Conjunta Negativa de Débitos\n' +
+      'Municipais e da Dívida Ativa do Município',
+    href: '#',
   },
   {
-    text: "Reemitir Certidão/Declaração",
-    href: "#",
+    text: 'Reemitir Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Validar Certidão/Declaração",
-    href: "#",
+    text: 'Reemitir Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Certidão de Baixa",
-    href: "#",
+    text: 'Validar Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Certidão de Estimativa",
-    href: "#",
+    text: 'Certidão de Baixa',
+    href: '#',
   },
   {
-    text: "Consultar Alvará de Funcionamento",
-    href: "#",
+    text: 'Certidão de Estimativa',
+    href: '#',
   },
   {
-    text: "Declaração de Inexistência de Cadastro Mercantil",
-    href: "#",
+    text: 'Consultar Alvará de Funcionamento',
+    href: '#',
   },
   {
-    text: "Portal de Parcelamento e Negociação",
-    href: "#",
+    text: 'Declaração de Inexistência de Cadastro Mercantil',
+    href: '#',
   },
   {
-    text: "Notificação de Instrução Normativa",
-    href: "#",
+    text: 'Portal de Parcelamento e Negociação',
+    href: '#',
   },
   {
-    text: "Sistema de Licenciamento - SLIC",
-    href: "#",
+    text: 'Notificação de Instrução Normativa',
+    href: '#',
   },
   {
-    text: "Piauí Digital",
-    href: "#",
+    text: 'Sistema de Licenciamento - SLIC',
+    href: '#',
   },
   {
-    text: "DESIF",
-    href: "#",
+    text: 'Piauí Digital',
+    href: '#',
   },
   {
-    text: "Sistema de Poda",
-    href: "#",
+    text: 'DESIF',
+    href: '#',
   },
   {
-    text: "Sistema Coleta de Lixo",
-    href: "#",
+    text: 'Sistema de Poda',
+    href: '#',
   },
   {
-    text: "Domicílio Tributário Eletrônico",
-    href: "#",
+    text: 'Sistema Coleta de Lixo',
+    href: '#',
   },
-];
+  {
+    text: 'Domicílio Tributário Eletrônico',
+    href: '#',
+  },
+]
 
 const cidadaoItems = [
-  { text: "Certidão Negativa de Débitos de IPTU", href: "#" },
+  { text: 'Certidão Negativa de Débitos de IPTU', href: '#' },
   {
-    text: "Certidão Positiva com Efeitos de Negativa IPTU",
-    href: "#",
+    text: 'Certidão Positiva com Efeitos de Negativa IPTU',
+    href: '#',
   },
   {
     text:
-      "Emitir Guia IPTU/TCRD-Taxa de Lixo/COSIP -\n" +
-      "Taxa de Iluminação Pública",
-    href: "#",
+      'Emitir Guia IPTU/TCRD-Taxa de Lixo/COSIP -\n' +
+      'Taxa de Iluminação Pública',
+    href: '#',
   },
   {
-    text: "Emitir Memória de Cálculo do Imóvel",
-    href: "#",
+    text: 'Emitir Memória de Cálculo do Imóvel',
+    href: '#',
   },
   {
-    text: "Emitir Guia ITBI-e",
-    href: "#",
+    text: 'Emitir Guia ITBI-e',
+    href: '#',
   },
   {
-    text: "Emitir Guia SIAT",
-    href: "#",
+    text: 'Emitir Guia SIAT',
+    href: '#',
   },
   {
-    text: "Sistema ITBI-e",
-    href: "#",
+    text: 'Sistema ITBI-e',
+    href: '#',
   },
   {
-    text: "Declaração de Inexistência de Imóvel",
-    href: "#",
+    text: 'Declaração de Inexistência de Imóvel',
+    href: '#',
   },
   {
-    text: "Declaração de Inexistência de Cadastro Mercantil",
-    href: "#",
+    text: 'Declaração de Inexistência de Cadastro Mercantil',
+    href: '#',
   },
   {
-    text: "Reemitir Certidão/Declaração",
-    href: "#",
+    text: 'Reemitir Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Validar Certidão/Declaração",
-    href: "#",
+    text: 'Validar Certidão/Declaração',
+    href: '#',
   },
   {
-    text: "Portal do IPTU",
-    href: "#",
+    text: 'Portal do IPTU',
+    href: '#',
   },
   {
-    text: "Portal de Negociação",
-    href: "#",
+    text: 'Portal de Negociação',
+    href: '#',
   },
   {
-    text: "Sistema Coleta de Lixo",
-    href: "#",
+    text: 'Sistema Coleta de Lixo',
+    href: '#',
   },
   {
-    text: "Sistema de Coleta de Poda",
-    href: "#",
+    text: 'Sistema de Coleta de Poda',
+    href: '#',
   },
   {
-    text: "Certidão de Número",
-    href: "#",
+    text: 'Certidão de Número',
+    href: '#',
   },
   {
-    text: "Declaração de Rua",
-    href: "#",
+    text: 'Declaração de Rua',
+    href: '#',
   },
   {
-    text: "Construa Fácil",
-    href: "#",
+    text: 'Construa Fácil',
+    href: '#',
   },
   {
-    text: "Cadastro como Tomador da Nota",
-    href: "#",
+    text: 'Cadastro como Tomador da Nota',
+    href: '#',
   },
-];
+]
 
 export {
   issItems,
@@ -392,4 +392,4 @@ export {
   imovelItens,
   empresaItens,
   cidadaoItems,
-};
+}
